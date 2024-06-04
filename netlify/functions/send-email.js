dde7dd05c2fb78066e8078f9feb8ef1ce84e4b29
strigeroscar <54800667+strigeroscar@users.noreply.github.com>
2024-06-04 23:18:34 +0200

    Initil commit

---
+++ netlify/functions/send-email.js
@@ -0,0 +1,35 @@
+// netlify/functions/send-email.js
+const nodemailer = require('nodemailer');
+
+exports.handler = async (event, context) => {
+  const { name, email, message } = JSON.parse(event.body);
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: 'oscarstrigerjobb@gmail.com',
+    subject: 'Contact Form Submission',
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Email sent successfully' }),
+    };
+  } catch (error) {
+    console.error('Error sending email:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Error sending email' }),
+    };
+  }
+};
--- src/services/emailService.jsx
+++ src/services/emailService.jsx
@@ -1,6 +1,6 @@
 // src/services/emailService.js
-export const sendEmail = async (emailData) => {//skickar ett POST-anrop till http://localhost:5000/send-email med e-postdata i JSON-format
-    const response = await fetch('http://localhost:5000/send-email', {
+export const sendEmail = async (emailData) => {//skickar ett POST-anrop till http://localhost:5000/send-email eller /.netlify/functions/send-email med e-postdata i JSON-format
+    const response = await fetch('/.netlify/functions/send-email', {///.netlify/functions/send-email http://localhost:5000/send-email
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
